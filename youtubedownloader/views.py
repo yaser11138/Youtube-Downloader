a90d9bb5a585c0118e3e8120f9ec3add72feba49
yaser11138 <ydahgan@gmail.com>
2022-03-06 11:03:38 +0330

    add forntend and backend

--- youtubedownloader/views.py
+++ youtubedownloader/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+import youtube_dl
+import datetime
+
+
+def download_video(request):
+    if request.method == 'POST':
+        video_url = request.POST['url']
+        with youtube_dl.YoutubeDL({}) as yd:
+            video_info = yd.extract_info(video_url, download=False)
+            context = {"title": video_info["title"],
+                       "audio": video_info["formats"][0]["url"],
+                       "video": video_info["formats"][-1]["url"],
+                       "upload": video_info["upload_date"],
+                       "duration": convert(video_info["duration"]),
+                       "thumbnail": video_info["thumbnail"],
+                       }
+        return render(request, "h.html", context=context)
+    else:
+        return render(request,"index.html",context={})
+
+# convert seconds to 00:00:00 time format
+def convert(seconds):
+    return str(datetime.timedelta(seconds=seconds))
+
+
